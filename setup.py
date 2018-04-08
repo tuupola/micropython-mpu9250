1e2ca333e69ba585f41bc8061bf747db5c1ccb05
Mika Tuupola <tuupola@appelsiini.net>
2018-04-08 14:02:06 +0700

    Use README.md as long description

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 import sys
 sys.path.pop(0)
 from setuptools import setup
+from codecs import open
+from os import path
+
+cwd = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(cwd, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="micropython-mpu9250",
     py_modules=["mpu9250", "mpu6500", "ak8963"],
     version="0.2.0",
     description="MicroPython I2C driver for MPU9250 9-axis motion tracking device",
-    long_description="MPU-9250 is a System in Package (SiP) which combines two chips: MPU-6500 which contains 3-axis gyroscope and 3-axis accelerometer and an AK8963 which is a 3-axis digital compass.",
-    keywords="accelerometer, gyro, magnetometer, micropython i2c",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="accelerometer, gyro, magnetometer, micropython, i2c",
     url="https://github.com/tuupola/micropython-mpu9250",
     author="Mika Tuupola",
     author_email="tuupola@appelsiini.net",
